92a0b089d42d371a4b36317fa1bdc17b5a0edf9d
Ivan Steblenko <akudm2007@yandex.ru>
2018-03-22 18:21:02 +0400

    add components

---
+++ app/components/header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export class Header extends React.Component<null,null> {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            
+            </nav>
+        );
+    }
+}
